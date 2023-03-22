6cc3aeee1d5b5ab0b8a0077ee6ece8f0ec3e4d7a
Roy Moran <119548052+RoyMoran210@users.noreply.github.com>
2023-03-22 18:47:38 -0500

    Update securedRoutes.js

--- React/securedRoutes.js
+++ React/securedRoutes.js
@@ -1,19 +1,10 @@
 import { lazy } from "react";
-const EmergencyContactsForm = lazy(() =>
-  import("../components/emergencycontacts/EmergencyContactForm")
-);
-const Orders = lazy(() => import("../components/orders/Orders"));
-const OrdersInfo = lazy(() => import("../components/orders/OrdersInfo"));
-const AdminDashBoard = lazy(() =>
-  import("../components/dashboards/default/index")
-);
 const PageNotFound = lazy(() => import("../components/errors/Error404"));
 const ProductsForm = lazy(() => import("../components/products/ProductsForm"));
 
 // dashboard
 const BlogForm = lazy(() => import("../components/blogs/BlogAdminForm"));
 
-const PageVisits = lazy(() => import("../components/gadashboard/PageVisits"));
 const dashboardRoutes = [
   {
     path: "/dashboard",
@@ -40,55 +31,6 @@ const dashboardRoutes = [
   },
 ];
 
-const emergencyContactRoutes = [
-  {
-    path: "/emergency/contact/form",
-    name: "EmergencyContacts",
-    exact: true,
-    element: EmergencyContactsForm,
-    roles: ["Admin"],
-    isAnonymous: false,
-  },
-];
-
-const orders = [
-  {
-    path: "/orders",
-    name: "Orders",
-    exact: true,
-    element: Orders,
-    roles: [],
-    isAnonymous: true,
-  },
-  {
-    path: "/orders/:id",
-    name: "Orders/id",
-    exact: true,
-    element: OrdersInfo,
-    roles: [],
-    isAnonymous: true,
-  },
-];
-
-const test = [
-  {
-    path: "/test",
-    name: "Test",
-    exact: true,
-    element: AdminDashBoard,
-    roles: ["Fail"],
-    isAnonymous: false,
-  },
-  {
-    path: "/secured2",
-    name: "A Secured Route",
-    exact: true,
-    element: AdminDashBoard,
-    roles: ["Admin"],
-    isAnonymous: false,
-  },
-];
-
 const products = [
   {
     path: "/products/new",
@@ -111,23 +53,11 @@ const errorRoutes = [
   },
 ];
 
-const blogAdminRoute = [
-  {
-    path: "/blogs/admin",
-    name: "adminForm",
-    exact: true,
-    element: BlogForm,
-    roles: [],
-  },
-];
 
 const allRoutes = [
   ...dashboardRoutes,
   ...test,
   ...errorRoutes,
-  ...emergencyContactRoutes,
-  ...orders,
-  ...blogAdminRoute,
   ...products,
 ];
 
